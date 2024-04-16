7d099017c29926a2670a9650bf762e519ba68baa
kpsshankar <kpsshankar@yahoo.co.in>
2024-04-16 17:37:26 +0530

    Drag_Drop.py

--- Drag_Drop.py
+++ Drag_Drop.py
@@ -1,21 +1,60 @@
-import time
-
-
 from selenium import webdriver
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from time import sleep
+
+# action chains
+from selenium.webdriver import ActionChains
 
 
+class DragAndDrop:
 
+    def __init__(self, url):
+        self.url = url
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        self.action = ActionChains(self.driver)
 
-url = "https://jqueryui.com/droppable/"
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.switch_to.frame(driver.find_element(by=By.TAG_NAME, value="iframe"))
+    def boot(self):
+        self.driver.get(self.url)
+        sleep(3)
+        self.driver.maximize_window()
 
+    def quit(self):
+        self.driver.quit()
 
-actions = ActionChains(driver)
-actions.drag_and_drop(driver.find_element(by=By.ID, value="draggable"), driver.find_element(by=By.ID, value="droppable")).perform()
-time.sleep(3)
-driver.quit()
+    def draggable(self, draggable):
+        fullXPath = "/html/body/div[1]"
+        element = self.driver.find_element(by=By.XPATH, value=fullXPath)
+        element.send_keys(draggable)
+        sleep(5)
+
+    def droppable(self, droppable):
+        fullXPath = "/html/body/div[2]"
+        element = self.driver.find_element(by=By.XPATH, value=fullXPath)
+        element.send_keys(droppable)
+        sleep(5)
+
+    def dragAndDrop(self,draggable):
+        try:
+            self.boot()
+            self.draggable()
+            self.droppable()
+            self.action.drag_and_drop(draggable, droppable).perform()
+            sleep(3)
+            fullXPath = "/html/body/div[2]/p"
+            droptext = self.driver.find_element(by=By.XPATH, value=fullXPath)
+        except  droptext == "Dropped!":
+            print("Success")
+        else:
+            print("Error")
+
+    #except NoSuchElementException as e:
+    #print(e)
+
+        finally:
+            self.driver.quit()
+
+obj = DragAndDrop("https://jqueryui.com/droppable/")
+#obj.dragAndDrop()
